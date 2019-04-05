e4cf7712b432f32d966126396bea1e87a50150c5
leovene <leo.ochoalopes@gmail.com>
2019-04-05 18:13:21 -0300

    Setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,18 +9,25 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueAxios,axios);
+
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
+import 'vuetify/dist/vuetify.min.css';
 
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('home-component', require('./components/HomeComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
